346875bd5b68c08026838fc2d72d68ebd93ed8d0
Eric Kanguzu <ekkanguzu@gmail.com>
2025-10-15 13:20:40 -0500

    Added script to get a list of vlans from config string

---
+++ scripts/get_list_of_vlans.py
@@ -0,0 +1,6 @@
+
+config = "switchport trunk allowed vlan 1,3,10,20,30,100"
+vlans_string = config.split()[-1]
+vlans_list = vlans_string.split(",")
+print(vlans_list)
+# Output: ['1', '3', '10', '20', '30', '
