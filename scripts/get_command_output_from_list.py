82854c33312c38dae40bd051577b054925c4d058
Eric Kanguzu <ekkanguzu@gmail.com>
2025-10-15 17:56:36 -0500

    Added a line to pass arguments when the script is run via terminal

--- scripts/get_command_output_from_list.py
+++ scripts/get_command_output_from_list.py
@@ -1,10 +1,16 @@
 
+from sys import argv
+# this script prints a switchport access template and adds a VLAN number to the second line of the template
+# the VLAN number is provided as a command line argument
+
+interface = argv[1]
+vlan = argv[2]
+
 access_template = ['switchport mode access',
 'switchport access vlan {}',
 'switchport nonegotiate',
 'spanning-tree portfast',
 'spanning-tree bpduguard enable']
 
-# this line prints the template and adds VLAN 5 to the second line of the template
-print('\n'.join(access_template).format(5))
-
+print("interface {}".format(interface))
+print("\n".join(access_template).format(vlan))
