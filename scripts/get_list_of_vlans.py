8e7ec04f62c34a5d3951ca0ab3691cac22713070
Eric Kanguzu <ekkanguzu@gmail.com>
2025-10-15 13:23:29 -0500

    Added last item of the list to the output comment line and fixed its bracket

--- scripts/get_list_of_vlans.py
+++ scripts/get_list_of_vlans.py
@@ -3,4 +3,4 @@ config = "switchport trunk allowed vlan 1,3,10,20,30,100"
 vlans_string = config.split()[-1]
 vlans_list = vlans_string.split(",")
 print(vlans_list)
-# Output: ['1', '3', '10', '20', '30', '
+# Output: ['1', '3', '10', '20', '30', '100']
