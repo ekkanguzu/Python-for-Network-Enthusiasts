763ba7b20665f6db1711809cdc77d739ed97b531
Eric Kanguzu <ekkanguzu@gmail.com>
2025-10-20 17:06:30 -0500

    Added functionality to skip lines that contain words to ignore

--- scripts/get_switch_command.py
+++ scripts/get_switch_command.py
@@ -5,6 +5,9 @@
 
 from sys import argv
 
+# script will skip lines that inlcude these words
+ignore_list = ["duplex", "alias", "Current configuration"]
+
 # get the file name from command line argument and construct the full path
 file_name = 'data/' + argv[1]
 
@@ -12,9 +15,20 @@ file_name = 'data/' + argv[1]
 with open(file_name) as f:
     for line in f:
         line = line.strip()
-
+        
         # skip lines that start with "!"
         if line.startswith("!"):
+            continue
+        
+        # Check each word in ignore_list
+        skip_line = False
+        for word in ignore_list:
+            if word in line:
+                skip_line = True
+                break  # Exit loop early if match found
+        
+        # If skip_line is True, skip this line
+        if skip_line:
             continue
 
         # print the remaining lines
