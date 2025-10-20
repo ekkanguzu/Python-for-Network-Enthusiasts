48bc2069c91305d8ab983d00d397d1f7bc63850e
Eric Kanguzu <ekkanguzu@gmail.com>
2025-10-20 15:15:35 -0500

    Modified script to work with nested dictionary

--- scripts/working_with_example_2.py
+++ scripts/working_with_example_2.py
@@ -6,15 +6,28 @@ with open('data/sh_ip_interface.txt') as f:
     for line in f:
         # check for lines that contain 'line protocol'
         if 'line protocol' in line:
+            
             # extract the interface name
             interface = line.split()[0]
+
+            # initialize the dictionary for this interface
+            result[interface] = {}
+        
+        # check for lines that contain 'Internet address'
+        elif "Internet address" in line:
+            # extract the IP address
+            ip_address = line.split()[-1]
+
+            # store the IP address in the dictionary
+            result[interface]['ip'] = ip_address
         
         # check for lines that contain 'MTU is'
         elif 'MTU is' in line:
             # extract the MTU value
             mtu = line.split()[-2]
-            # add the interface and MTU to the dictionary
-            result[interface] = mtu
+
+            # store the MTU value in the dictionary
+            result[interface]['mtu'] = mtu
 
 # print the resulting dictionary
 print(result)
